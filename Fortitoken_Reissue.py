a5c54fdd88e3db1a0fc34d53c31db688f3b345a6
Rel1ct0 <heupomaht@gmail.com>
2021-08-12 11:19:29 +0300

    Update Fortitoken_Reissue.py

--- Fortitoken_Reissue.py
+++ Fortitoken_Reissue.py
@@ -2,7 +2,7 @@ import requests
 import sys
 import json
 import warnings
-
+import time
 
 newToken = {
     "active": True,
@@ -11,31 +11,36 @@ newToken = {
     'token_type': 'ftm'
 }
 
-
 SkipIf = 'OU=Disabled'
-
-
-if len(sys.argv) != 4:
-    print("Syntax: ./Fortitoken_Reissue <FortiAuthIP> <ApiAdminUsername> <ApiAdminKey>")
-    exit(1)
 
 warnings.filterwarnings("ignore")
 FortiAuth = sys.argv[1]
 user = sys.argv[2]
 apiKey = sys.argv[3]
 
+logfile = open('/home/glushkovpa/token_reissue.log', 'a')
+logfile.write(f'\n\n=============={time.asctime(time.localtime())}================\n\n')
 
-print('Getting a list of inactive users')
-userList = requests.get(f'https://{FortiAuth}/api/v1/ldapusers/?format=json&limit=10000&active=false',
+logfile.write('Getting a list of inactive users\n')
+response = requests.get(f'https://{FortiAuth}/api/v1/ldapusers/?format=json&limit=10000&active=false',
                         auth=(user, apiKey),
                         verify=False)
 
-if userList.status_code > 299:
-    print(f"Error, got {userList.status_code}")
+if response.status_code > 299:
+    logfile.write(f"Error, got {response.status_code}\n")
+    logfile.close()
     exit(1)
 
-userList = userList.json()['objects']
-print(f'Found {len(userList)} inactive users')
+userList = response.json()['objects']
+
+while response.json()['meta']['next']:
+    nextData = response.json()['meta']['next']
+    response = requests.get(f'https://{FortiAuth}{nextData}',
+                        auth=(user, apiKey),
+                        verify=False)
+    userList.extend(response.json()['objects'])
+
+logfile.write(f'Found {len(userList)} inactive users\n')
 
 usersToReissue = list()
 
@@ -43,40 +48,56 @@ for _ in userList:
     if SkipIf not in _['dn']:
         usersToReissue.append(_)
 
-print(f'Found {len(usersToReissue)} inactive users that will have their Tokens reissued')
+if not usersToReissue:
+    logfile.write('No users eligible for token reissue\n')
+    logfile.close()
+    exit(0)
 
-tokens = requests.get(f'https://{FortiAuth}/api/v1/fortitokens/?format=json&limit=10000&status=available',
+logfile.write(f'Found {len(usersToReissue)} inactive users that will have their Tokens reissued\n')
+
+response = requests.get(f'https://{FortiAuth}/api/v1/fortitokens/?format=json&limit=10000&status=available',
                       auth=(user, apiKey),
                       verify=False)
-if tokens.status_code > 299:
-    print(f"Error, got {tokens.status_code}")
+if response.status_code > 299:
+    logfile.write(f"Error, got {response.status_code}\n")
+    logfile.close()
     exit(1)
 
-tokens = tokens.json()['objects']
-print(f'Found {len(tokens)} available tokens')
+tokens = response.json()['objects']
+
+while response.json()['meta']['next']:
+    nextData = response.json()['meta']['next']
+    response = requests.get(f'https://{FortiAuth}{nextData}',
+                        auth=(user, apiKey),
+                        verify=False)
+    tokens.extend(response.json()['objects'])
+
+
+logfile.write(f'Found {len(tokens)} available tokens\n')
 
 freetokens = list()
 
 for token in tokens:
-    if token['locked'] == False:
+    if not token['locked']:
         freetokens.append(token)
 
-print(f'Found {len(freetokens)} available and unlocked tokens')
+logfile.write(f'Found {len(freetokens)} available and unlocked tokens\n')
 
-print('Reissuing tokens...')
+logfile.write('Reissuing tokens...\n')
 
 usercounter = 0
 for nextuser in usersToReissue:
     newToken['token_serial'] = freetokens[usercounter]['serial']
     done = requests.patch(f"https://{FortiAuth}/api/v1/ldapusers/{nextuser['id']}/",
-                    data=json.dumps(newToken),
-                    auth=(user, apiKey),
-                    verify=False)
+                          data=json.dumps(newToken),
+                          auth=(user, apiKey),
+                          verify=False)
     usercounter = usercounter + 1
     if done.status_code < 300:
-        print(f'Ok, token reissued for {nextuser["username"]}')
+        logfile.write(f'Ok, token reissued for {nextuser["username"]}\n')
     else:
-        print(f"Got error {done.status_code} for user {nextuser['id']}")
-        print(done.content)
+        logfile.write(f'Got error {done.status_code} for user {nextuser["username"]}\n')
+        logfile.write(done.content)
 
-print(f'Reissued tokens for {usercounter} users')
+logfile.write(f'Reissued tokens for {usercounter} users\n')
+logfile.close()
